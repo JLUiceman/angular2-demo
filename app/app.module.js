bb2e8e0ac4f6f563e42d5a434cc344035f5974ad
JLUiceman <138885146@qq.com>
2016-08-23 13:54:52 +0800

    'change'

--- app/app.module.js
+++ app/app.module.js
@@ -2,8 +2,8 @@
   app.AppModule =
     ng.core.NgModule({
       imports: [ ng.platformBrowser.BrowserModule ],
-      declarations: [ app.AppComponent ,app.AppComponent2,app.liComponent],
-      bootstrap: [ app.AppComponent ,app.AppComponent2,app.liComponent]
+      declarations: [ app.AppComponent ,app.AppComponent2,app.liComponent,app.canvasComponent],
+      bootstrap: [ app.AppComponent ,app.AppComponent2,app.liComponent,app.canvasComponent]
     })
     .Class({
       constructor: function() {}
